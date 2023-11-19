465491ec29041a9f6c90b2704810f8b94f144edf
lapnq <ngoquanglap0612@gmail.com>
2023-11-19 16:14:40 +0700

    fix cors v3

--- config/index.js
+++ config/index.js
@@ -4,11 +4,11 @@ const app = express();
 
 const corsConfig = {
   // Configures the Access-Control-Allow-Origin
-  // origin:
-  //   app.get("env") === "production"
-  //     ? ("http://localhost:3000" || process.env.SERVER_API_URL)
-  //     : process.env.SERVER_API_URL,
-  origin: '*',
+  origin:
+    app.get("env") === "production"
+      ? ("http://localhost:3000" || process.env.CORS_ORIGIN)
+      : process.env.CORS_ORIGIN,
+  // origin: '*',
 
   // Configures the Access-Control-Allow-Methods
   methods: "GET, POST, OPTIONS, PUT, PATCH, DELETE",
